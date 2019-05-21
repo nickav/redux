cef13659d7b8a4437321012adbb743d8c13c0cb2
Nick Aversano <nick23hi@gmail.com>
2019-05-21 17:47:31 -0400

    implemented thunk with @anushriadhia

--- index.mjs
+++ index.mjs
@@ -26,6 +26,21 @@ const countReducer = (state = initialState, action) => {
   }
 }
 
+const wait = (ms) => new Promise((res) => setTimeout(res, ms));
+
+const incrementAction = (count) => ({ type: "increment", count });
+
+const thunkAction = () => (dispatch, getState) => {
+  wait(1000).then(() => {
+    dispatch(incrementAction(10));
+  });
+}
+
+const thunkAction2 = () => (dispatch) => {
+  dispatch(incrementAction(1));
+  dispatch(incrementAction(2));
+}
+
 // middlewares
 const logger = store => next => action => {
   console.log('will dispatch', action);
@@ -40,8 +55,19 @@ const logger = store => next => action => {
   return returnValue;
 }
 
+const thunk = store => next => action => {
+  if (typeof action === 'function') {
+    return action(store.dispatch, store.getState);
+  }
+  
+  return next(action);
+};
+
 const rootReducer = combineReducers({ counter: countReducer });
-const store = createStore(rootReducer, {}, applyMiddlewares([logger]));
+const store = createStore(rootReducer, {}, applyMiddlewares([ thunk, logger ]));
+
+console.log("initial state", store.getState());
 
 store.subscribe(() => console.log(`State changed!`, store.getState()));
-store.dispatch({ type: "increment", count: 1 });
+store.dispatch(incrementAction(1));
+store.dispatch(thunkAction2());
